676828ff02576a74960783ee7e12c19352d8933f
HiImFelipe <13539403+HiImFelipe@users.noreply.github.com>
2025-08-16 18:08:54 -0300

    refactor: update MockTaskRepository

--- src/test/mocks/repositories/MockTaskRepository.ts
+++ src/test/mocks/repositories/MockTaskRepository.ts
@@ -1,6 +1,9 @@
 import type { EntityId } from "@/shared/domain/Entity";
 import type { Task } from "@/task/domain/Task";
-import type { TaskRepository } from "@/task/domain/TaskRepository";
+import type {
+	GetAllTasksParams,
+	TaskRepository,
+} from "@/task/infra/repository/TaskRepository";
 
 export class MockTaskRepository implements TaskRepository {
 	private tasks: Map<EntityId, Task> = new Map();
@@ -14,10 +17,8 @@ export class MockTaskRepository implements TaskRepository {
 		return this.tasks.get(id) || null;
 	}
 
-	async findByProjectId(projectId: EntityId): Promise<Task[]> {
-		return Array.from(this.tasks.values()).filter(
-			(task) => task.projectId === projectId,
-		);
+	async findByTaskId(taskId: EntityId): Promise<Task[]> {
+		return Array.from(this.tasks.values()).filter((task) => task.id === taskId);
 	}
 
 	async update(task: Task): Promise<Task> {
@@ -29,8 +30,30 @@ export class MockTaskRepository implements TaskRepository {
 		this.tasks.delete(id);
 	}
 
-	async findAll(): Promise<Task[]> {
-		return Array.from(this.tasks.values());
+	async findByProjectId(projectId: EntityId): Promise<Task[]> {
+		return Array.from(this.tasks.values()).filter(
+			(task) => task.projectId === projectId,
+		);
+	}
+
+	async findAll(params: GetAllTasksParams): Promise<Task[]> {
+		const { search, limit, page } = params;
+
+		let tasks = Array.from(this.tasks.values());
+
+		if (search) {
+			tasks = tasks.filter((task) =>
+				task.title.toLowerCase().includes(search.toLowerCase()),
+			);
+		}
+
+		if (page && limit) {
+			const start = (page - 1) * limit;
+			const end = start + limit;
+			tasks = tasks.slice(start, end);
+		}
+
+		return tasks;
 	}
 
 	clear(): void {
