3a1403416548e89966ebe5dc2614e59424ae96c6
HiImFelipe <13539403+HiImFelipe@users.noreply.github.com>
2025-08-16 18:09:33 -0300

    refactor: remove unused exports from task controller index.ts file

--- src/task/controller/index.ts
+++ src/task/controller/index.ts
@@ -1,5 +1,2 @@
 export { DeleteTaskController } from "./DeleteTaskController";
-export { GetAllTasksController } from "./GetAllTasksController";
-export { GetTaskController } from "./GetTaskController";
-export { GetTasksByProjectController } from "./GetTasksByProjectController";
 export { UpdateTaskController } from "./UpdateTaskController";
